aac0d05e376e2b7f7342401dc3e0e8d4a2184a2c
Erika Imperial <imperial.erikajane@gmail.com>
2020-02-06 18:04:14 +0800

    Make a simple to do list using EJS module of NodeJS

---
+++ app.js
@@ -0,0 +1,55 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+const items = [];
+const workItems = [];
+
+app.get("/", function(req, res) {
+  let day = date.getDate();
+  res.render("list", {
+    listTitle: day,
+    newListItems: items
+  });
+});
+
+app.post("/", function(req, res) {
+  if (req.body.list === "Work") {
+    workItems.push(req.body.toDo);
+    res.redirect("/work");
+  } else {
+    items.push(req.body.toDo);
+    res.redirect("/");
+  }
+});
+
+app.get("/work", function(req, res) {
+  res.render("list", {
+    listTitle: "Work List",
+    newListItems: workItems
+  });
+});
+
+app.post("/work", function(req, res) {
+  workItems.push(req.body.toDo);
+  res.redirect("/work");
+});
+
+app.get("/about", function(req, res) {
+  res.render("about");
+});
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000.");
+});
